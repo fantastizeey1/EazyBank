6418eaee7d1a889456910e84f188609832f3814a
Your Name <fantastizeey@gmail.com>
2024-05-11 04:01:32 +0100

    Updated files

--- src/components/Design.jsx
+++ src/components/Design.jsx
@@ -12,11 +12,11 @@ function Design() {
     <div className="md:-mt-24 font-sans">
       <div className="bg-[#f3f4f6] ">
         <div className="md:ml-24 md:mr-24 md:w-[100%-144px] flex flex-col justify-center items-center md:justify-start md:items-start p-5 md:py-10 ">
-          <div className="md:w-[50%] w-[50%]  my-3 flex items-center md:items-start md:justify-start justify-center flex-col">
+          <div className="md:w-[50%] w-full  my-3 flex items-center md:items-start md:justify-start justify-center flex-col">
             <h2 className="md:my-4 text-[30px] flex items-center md:items-start md:justify-normal text-center md:text-start font-semibold">
               Why choose Easybank?
             </h2>
-            <p className=" font-normal flex items-center p-4 text-center md:text-start text-[#9698a6]">
+            <p className=" font-normal flex items-center p-2 md:p-4 text-center md:text-start text-[#9698a6]">
               We leverage Open Banking to turn your bank account into your
               financial hub. Control your finances like never before.
             </p>
